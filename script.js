0111dfe93d54b73b473bf0cb3ffb7a4b6b722ca7
unknown <Yahyaaitahmed20@gmail.com>
2024-07-12 22:04:53 +0100

    Made The Site

---
+++ script.js
@@ -0,0 +1,21 @@
+let container = document.querySelector('.container')
+DrawGrid()
+document.querySelector('#eraser').addEventListener("click",()=>{DrawGrid()})
+function DrawGrid(){
+    let resolution = prompt('write the resolution you want for your sketch')
+    if(resolution > 100){
+        return;
+    }
+    container.innerHTML = ''
+    for(let i = 0;i<resolution*resolution; i++){
+    let pixel = document.createElement('div');
+    pixel.setAttribute("style",
+    `
+        width : ${700/resolution}px;
+        height : ${700/resolution}px;
+        
+    `)
+    pixel.classList.add("pixel")
+    container.append(pixel)
+    }
+}
